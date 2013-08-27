ed7335f4f7dccbb67cdec55d510fbc9ba517fd65
Gijs Paepen <gijs.paepen@kla-tencor.com>
2013-08-27 11:42:35 +0200

    2

--- src/com/example/notifandservtest/MainActivity.java
+++ src/com/example/notifandservtest/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.notifandservtest;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,15 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void  onStartServiceClick(View view) {
+		Intent intent = new Intent(this, DcmService.class);
+		startService(intent);
+	}
+	
+	public void  onStopServiceClick(View view) {
+
 	}
 
 }
